fd5d2213aeab52ba6375bd9845674c2145e8ebc7
Tomiwa Babalola <tbabalola004@gmail.com>
2021-09-28 09:29:15 -0400

    hzn

--- Lab - Structures.playground/Pages/2. App Exercise - Workout Tracking.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/2. App Exercise - Workout Tracking.xcplaygroundpage/Contents.swift
@@ -17,7 +17,9 @@ struct RunningWorkout{
  Create a variable instance of `RunningWorkout` called `firstRun` without supplying any arguments. Print out all three properties of `firstRun`. This is a good example of when using default values is appropriate, seeing as all running workouts start with a distance, time, and elevation change of 0.
  */
 var firstRun = RunningWorkout(distance: 0.0, time: 1200, elevation: 0)
+firstRun.distance = 9.0
 
+print(firstRun.distance)
 /*:
  Now imagine that throughout the course of the run, you go a distance of 2,396 meters in 15.3 minutes, and gain 94 meters of elevation. Update the values of `firstRun`'s properties accordingly. Print a statement about your run using the values of each property.
  */
